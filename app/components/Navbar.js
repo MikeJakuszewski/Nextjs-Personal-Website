935ea373c8c7d194c683a572facb37e22152170a
Mike Jakuszewski <michael_jakuszewski@hotmail.com>
2023-10-02 16:10:41 -0400

    initial commit

---
+++ app/components/Navbar.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Logo from "./Logo";
+
+const Navbar = () => {
+  return (
+    <div className="w-full px-32 py-8 font-medium flex items-center justify-between">
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/portfolio">Portfolio</Link>
+      </nav>
+      <Logo />
+      <nav>
+        <Link href="/" target="_blank">
+          T
+        </Link>
+        <Link href="/" target="_blank">
+          T
+        </Link>
+        <Link href="/" target="_blank">
+          T
+        </Link>
+        <Link href="/" target="_blank">
+          T
+        </Link>
+      </nav>
+    </div>
+  );
+};
+export default Navbar;
